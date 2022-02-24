2bc75a225484c252fe0d927e7d30201733dce587
Ttack412 <31516693+Ttack412@users.noreply.github.com>
2022-02-23 19:44:09 -0500

    Add files via upload

---
+++ DataTypes/Main.java
@@ -0,0 +1,102 @@
+class Main {
+  public static void main(String[] args) {
+//-------------------------------------------
+    byte aByte = -128;
+    System.out.println(aByte);
+//-------------------------------------------
+    
+    // 1. Create a byte with the largest byte value possible and print it out.
+ 
+   byte largestByte = 127;
+   System.out.println(largestByte);
+
+
+    // 2. Try making a byte with a value greater than 127 or less than -128. Print it out and see what happens! Remove this byte for the test.
+
+   //byte tooBigByte = 500;
+   //System.out.println(tooBigByte);
+
+
+//-------------------------------------------
+    short aShort = 32766;
+    System.out.println(aShort);
+
+    int anInt = -56;
+    System.out.println(anInt);
+//-------------------------------------------    
+    // 3. Create an int with the smallest int value possible.
+
+    int smallestInt = -2147483648;
+
+
+    // 4. Print it out
+
+    System.out.println(smallestInt);
+
+
+//-------------------------------------------
+    long aLong = 9223375807L;
+    System.out.println(aLong);
+
+    float aFloat = 9.99f;
+    System.out.println(aFloat);
+//-------------------------------------------
+
+    // 5. Create a float with the value 1.0111f.
+
+    float myFloat = 1.0111f;
+
+    // 6. Print it out.
+
+    System.out.println(myFloat);
+
+
+//-------------------------------------------
+    double aDouble = 99.98d;
+    System.out.println(aDouble);
+
+    boolean aBoolean = true;
+    System.out.println(aBoolean);
+//-------------------------------------------
+
+    // 7. Create a boolean that is set to false.
+
+    boolean myBool = false;
+
+    // 8. Print it out
+
+    System.out.println(myBool);
+
+
+//-------------------------------------------
+    char aChar = 'a';
+    System.out.println(aChar);
+//-------------------------------------------
+    // 9. Create a char with the value of '1'
+
+    char myChar = '1';
+
+
+    // 10. Print it out
+
+    System.out.println(myChar);
+
+
+//-------------------------------------------
+    String aString = "the quick brown fox jumps over the lazy dog";
+    System.out.println(aString);
+//-------------------------------------------
+
+    // 11. Create a string with the phrase: "I mastered Java data types!"
+
+  
+    String myString = "I mastered Java data types!";
+
+
+    // 12. Print it out.
+    
+    System.out.println(myString);
+
+
+  }
+}
